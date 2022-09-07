f83d4e531e8322db95bb64d3cd884fbfbb37ce7e
Sejung Kwak <kwak.sejung@gmail.com>
2022-09-07 14:39:33 +0000

    feat: Create adding item functionality

--- todolist/todo/views.py
+++ todolist/todo/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Todo
 
 
 def index(request):
+
+    if request.method == 'POST':
+        new_todo = Todo(
+            title = request.POST['title']
+        )
+        new_todo.save()
+        return redirect('/')
+
     return render(request, 'index.html')
